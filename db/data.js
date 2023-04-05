fd809916ce8f03ddaf62e2da50761e896186cd8c
100Niraj <nirajkumarpatel264@gmail.com>
2023-04-06 00:07:57 +0530

    g

--- db/data.js
+++ db/data.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB  = " mongodb+srv://nirajkumarpatel264:9060979789@cluster0.mjvnllr.mongodb.net/?retryWrites=true&w=majority ";
+const DB  = "mongodb+srv://nirajkumarpatel264:9060979789@cluster0.mjvnllr.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(DB ,{
     // useNewUrlParser : true,
